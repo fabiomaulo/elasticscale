220d64db5a93400e55565cb56ea29b95a12eb841
Fabio Maulo <fabiomaulo@gmail.com>
2016-10-13 18:03:22 -0300

    Entities

---
+++ Slider.WorkDone/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace Slider.WorkDone
+{
+	public class Project: Entity
+	{
+		public Project()
+		{
+			Active = true;
+		}
+		public Company Owner { get; set; }
+		public string Name { get; set; }
+		public bool Active { get; set; }
+	}
+}
---
+++ Slider.WorkDone/Workdone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Slider.WorkDone
+{
+	public class Workdone:Entity
+	{
+		public Workdone()
+		{
+			Tags=new List<string>();
+		}
+
+		public Project Project { get; set; }
+		public Professional Professional { get; set; }
+		public string Description { get; set; }
+		public DateTime Day { get; set; }
+		public TimeSpan Duration { get; set; }
+		public IList<string> Tags { get; }
+		public bool Invoiced { get; set; }	
+
+		public void SetTagsFromCsv(string source)
+		{
+			Tags.Clear();
+			if (source == null)
+			{
+				return;
+			}
+			var validTags = source.Split(',').Select(t=> t.Trim()).Where(t=> !string.IsNullOrWhiteSpace(t));
+			foreach (var tag in validTags)
+			{
+				Tags.Add(tag);
+			}			
+		}
+	}
+}
