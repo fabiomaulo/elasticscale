6842a5f7c186b4f7dd1db620ad1e759babbcb0d6
Fabio Maulo <fabiomaulo@gmail.com>
2016-10-13 17:57:22 -0300

    Domain events

---
+++ Slider.WorkDone/Events/IDomainEventHandler.cs
@@ -0,0 +1,11 @@
+namespace Slider.WorkDone.Events
+{
+	/// <summary>
+	///   Contract for DomainEvent handler.
+	/// </summary>
+	/// <typeparam name="T">The type of domain event.</typeparam>
+	public interface IDomainEventHandler<T> where T : IDomainEvent
+	{
+		void Handle(T args);
+	}
+}
