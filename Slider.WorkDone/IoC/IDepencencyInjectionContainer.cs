6ce492739bd2d7a680e7895174c25ed7a4acb574
Fabio Maulo <fabiomaulo@gmail.com>
2016-10-13 17:55:46 -0300

    IoC Container

---
+++ Slider.WorkDone/IoC/IDepencencyInjectionContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slider.WorkDone.IoC
+{
+	public interface IDepencencyInjectionContainer : IDisposable
+	{
+		T GetInstance<T>() where T : class;
+		object GetInstance(Type type);
+
+		IEnumerable<T> GetInstances<T>() where T : class;
+		IEnumerable<object> GetInstances(Type type);
+	}
+}
