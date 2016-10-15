7b5430c54ef761c8954f9c3615f51ad47bc8bf9a
Fabio Maulo <fabiomaulo@gmail.com>
2016-10-15 15:10:28 -0300

    Tenant create with its shard

---
+++ Slider.WorkDone/Data/InitializeCommon.sql
@@ -0,0 +1,12 @@
+﻿IF OBJECT_ID('Tenants', 'U') IS NULL 
+BEGIN
+	CREATE TABLE Tenants(
+		Id uniqueidentifier NOT NULL,
+		[Owner] nvarchar(100) NOT NULL,
+		Name nvarchar(140) NOT NULL,
+		[Level] varchar(20) NOT NULL,
+	 CONSTRAINT PK_Tenants PRIMARY KEY CLUSTERED (Id)
+	)
+	CREATE NONCLUSTERED INDEX IX_Tenants_Owner ON Tenants([Owner] ASC)
+END
+GO
--- Workdone/Global.asax.cs
+++ Workdone/Global.asax.cs
@@ -1,4 +1,12 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Azure.SqlDatabase.ElasticScale.ShardManagement;
+using Microsoft.Azure.SqlDatabase.ElasticScale.ShardManagement.Schema;
+using Slider.WorkDone.Data.ElasticScale;
 using Slider.WorkDone.IoC;
 
 namespace Slider.WorkDone.Api
@@ -11,6 +19,51 @@ namespace Slider.WorkDone.Api
 		{
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			GlobalConfiguration.Configure(c => container.Register(c));
+			InitializeShardMap();
+		}
+
+		private void InitializeShardMap()
+		{
+			var conf = container.GetInstance<MultiverseConfiguration>();
+			var dataBase = container.GetInstance<DbFacility>();
+			CreateShardMapDbWhereNeeded(conf, dataBase);
+			var smm = container.GetInstance<ShardMapManager>();
+			var shards = container.GetInstance<SmmFacility>();
+			var shardMaps = smm.GetSchemaInfoCollection();
+			shards.CreateOrGetListShardMap<Guid>(smm, conf.TenantsShardMapName);
+			if (shardMaps.Any(x => x.Key == conf.TenantsShardMapName))
+			{
+				// Acá abría que efectuar modificaciones al schema del sharding
+				return;
+			}
+			CreateSchemaInfo(smm, conf.TenantsShardMapName);
+			CreateCommonShema(conf, dataBase);
+		}
+
+		private static void CreateSchemaInfo(ShardMapManager smm, string shardMapName)
+		{
+			SchemaInfo schemaInfo = new SchemaInfo();
+			schemaInfo.Add(new ShardedTableInfo("Companies", "TenantId"));
+			schemaInfo.Add(new ShardedTableInfo("Professionals", "TenantId"));
+			schemaInfo.Add(new ShardedTableInfo("Worksdone", "TenantId"));
+
+			smm.GetSchemaInfoCollection().Add(shardMapName, schemaInfo);
+		}
+
+		private void CreateCommonShema(MultiverseConfiguration conf, DbFacility database)
+		{
+			var connectionString = database.GetConnectionString(conf.ShardMapManagerServerName, conf.ShardMapManagerDatabaseName);
+			var assembly = typeof(DbFacility).Assembly;
+			var resourceName = "Slider.WorkDone.Data.InitializeCommon.sql";
+			DbFacility.ExecuteSqlScript(connectionString, assembly.GetManifestResourceStream(resourceName)).Wait();
+		}
+
+		private static void CreateShardMapDbWhereNeeded(MultiverseConfiguration conf, DbFacility database)
+		{
+			if (!database.Exists(conf.ShardMapManagerServerName, conf.ShardMapManagerDatabaseName).Result)
+			{
+				var created = database.CreateDatabase(conf.ShardMapManagerServerName, conf.ShardMapManagerDatabaseName, waitIsOnLine: true).Result;
+			}
 		}
 	}
 }
