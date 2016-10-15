0d10504ff24ae41be3e5d4ce9c096ab270abcb53
Fabio Maulo <fabiomaulo@outlook.com>
2016-10-15 16:13:24 -0300

    Ups get subscription

--- Slider.WorkDone/Data/ITenantPersister.cs
+++ Slider.WorkDone/Data/ITenantPersister.cs
@@ -5,6 +5,7 @@ namespace Slider.WorkDone.Data
 {
 	public interface ITenantPersister
 	{
+		Task<Tenant> Get(Guid id);
 		Task<bool> Exists(string email);
 		Task<bool> IsAvailable(Guid id);
 		Task Create(Tenant tenant);
